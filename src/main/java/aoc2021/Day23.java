0f53d9f2bb6d4bc2309e72bac12ea0219d6a9c7b
alxfhl <110429902+alxfhl@users.noreply.github.com>
2023-12-30 00:07:39 +0100

    Day 23

---
+++ src/main/java/aoc2021/Day23.java
@@ -0,0 +1,10 @@
+package aoc2021;
+
+public class Day23 {
+
+    public static void main(String[] args) {
+        // i actually solved these by hand / notepad++
+        System.out.println("part 1: 19059");
+        System.out.println("part 2: 48541");
+    }
+}
